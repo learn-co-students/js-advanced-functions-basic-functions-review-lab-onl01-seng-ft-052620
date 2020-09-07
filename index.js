a67d9565e656c235b7fb7a81e6cef9ca18412b1e
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-09-07 14:33:09 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(par = '*'){
+    return function inner(word = "bo,yea"){
+        return `You are ${par}${word}${par}!`
+    }
+}
+let Calculator = {
+    add: (arg1,arg2) => {
+        return arg1 + arg2
+    },
+    subtract: (arg1,arg2) => {
+        return arg1 - arg2
+    },
+    multiply: (arg1,arg2) => {
+        return arg1 * arg2
+    },
+    divide: (arg1,arg2) => {
+        return arg1 / arg2
+    }
+}
+
+function actionApplyer(integer, functions) {
+
+    for (let i=0; i<functions.length; i++){
+        integer = functions[i](integer)
+    }
+
+    return integer
+ }  
