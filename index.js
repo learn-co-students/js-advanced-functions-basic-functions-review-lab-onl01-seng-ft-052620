41e827348c313c87f409e4b66e688d61e2ec239e
ejguzman3988 <eriberto.guzman@outlook.com>
2020-09-14 00:08:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+function saturdayFun(str = 'roller-skate'){
+    return `This Saturday, I want to ${str}!`
+}
+
+function mondayWork(str = 'go to the office'){
+    return `This Monday, I will ${str}.`
+}
+
+let wrapAdjective = function(str = '*'){
+    return function(param = 'special'){
+        return `You are ${str}${param}${str}!`
+    }
+}
+
+const Calculator = {
+    add(a,b){
+        return a + b
+    },
+    subtract(a,b){
+        return a-b
+    },
+    multiply(a,b){
+        return a*b
+    },
+    divide(a,b){
+        return a/b
+    }
+}
+
+function actionApplyer(start, arrayF){
+    if (arrayF.length === 0){
+        return start;
+    }
+    else{
+        let newObj = Object.assign(start)
+        arrayF.forEach(f => {
+            newObj = f(newObj);
+        });
+        return newObj
+        
+    }
+}
+
